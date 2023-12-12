fa3818c744cdd84c179c080713025ebbc0a554ea
Kacper486 <153562714+Kacper486@users.noreply.github.com>
2023-12-13 00:22:42 +0100

    Update Ustawienia3.js

--- src/pages/Ustawienia3.js
+++ src/pages/Ustawienia3.js
@@ -1,9 +1,13 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import PasswordFormContainer from "../components/PasswordFormContainer";
 import "./Ustawienia3.css";
 
 const Ustawienia3 = () => {
   const navigate = useNavigate();
+  const [password, setPassword] = useState("");//obecne
+  const [password2, setPassword2] = useState("");//nowe
+  const [password3, setPassword3] = useState("");//potwierdz
 
   const onIkonaPowiadomieClick = useCallback(() => {
     navigate("/powiadomienia");
@@ -17,13 +21,17 @@ const Ustawienia3 = () => {
     navigate("/ustawienia");
   }, [navigate]);
 
-  const onPrzyciskiWyboruClick = useCallback(() => {
-    navigate("/ustawienia");
-  }, [navigate]);
-
   const onPrzyciskiWyboru1Click = useCallback(() => {
     navigate("/ustawienia");
   }, [navigate]);
+
+  const onPrzyciskiWyboruClick = useCallback(() => {
+    if (password2 == password3 && password == password/*tutaj porownanie z bazą */) {
+      navigate("/ustawienia");
+    } else {
+      alert("Hasła nie są takie same");
+    }
+  }, [password2, password3, navigate]);
 
   return (
     <div className="ustawienia-3">
@@ -58,15 +66,35 @@ const Ustawienia3 = () => {
       <img className="ustawienia-3-child" alt="" src="/ellipse-212@2x.png" />
       <div className="danka12">@danka12</div>
       <b className="danuta">Danuta</b>
-      <div className="wprowad-obecne-haso">Wprowadź obecne hasło:</div>
-      <img className="ustawienia-3-item" alt="" src="/vector-9.svg" />
-      <div className="ustawienia-3-inner" />
-      <div className="wprowad-nowe-haso">Wprowadź nowe hasło:</div>
+      <div className="wprowad-obecne-haso">Wprowadź obecne hasło:
+      <PasswordFormContainer
+              inputType="Password"
+              credentialsInput=""
+              inputLabel="Wprowadź obecne hasło"
+              onInputChange={(value) => setPassword(value)}
+              propTop="0rem"
+      />
+      </div>
+      <div className="wprowad-nowe-haso">Wprowadź nowe hasło:
+      <PasswordFormContainer
+              inputType="Password"
+              credentialsInput=""
+              inputLabel="Wprowadź nowe hasło"
+              onInputChange={(value) => setPassword2(value)}
+              propTop="0rem"
+      />
+      </div>
       <img className="vector-icon" alt="" src="/vector-10.svg" />
-      <div className="div">******************</div>
-      <div className="potwierd-nowe-haso">Potwierdź nowe hasło:</div>
+      <div className="potwierd-nowe-haso">Potwierdź nowe hasło:
+      <PasswordFormContainer
+              inputType="Password"
+              credentialsInput=""
+              inputLabel="Potwierdź nowe hasło"
+              onInputChange={(value) => setPassword3(value)}
+              propTop="0rem"
+      />
+      </div>
       <img className="ustawienia-3-child1" alt="" src="/vector-10.svg" />
-      <div className="div1">*****************</div>
       <button className="przyciski-wyboru" onClick={onPrzyciskiWyboruClick}>
         <b className="zapisz-zmiany">Zapisz zmiany</b>
         <img className="sign-out-squre-fill-icon1" alt="" src="/vector2.svg" />
